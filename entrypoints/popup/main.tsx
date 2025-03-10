a6cdf07883499a634e0d65a59f823901a2000f75
Jiang <jiangliangiobit@gmail.com>
2025-03-10 14:18:14 +0800

    feat: 提交修改

--- entrypoints/popup/main.tsx
+++ entrypoints/popup/main.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './style.css';
+import MainLayout from "@/src/component/main-layout.tsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <MainLayout />
   </React.StrictMode>,
 );
